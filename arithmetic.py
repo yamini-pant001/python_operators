2cf7a49b37896c1e5b7f6eda5b20984db3fbc1cd
yamini-pant001 <yaminipant001@gmail.com>
2025-04-29 10:36:44 +0530

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,17 @@
+#  Arithmetic Operators
+
+a=34
+b=2
+
+print("a+b=",a+b)       #addition
+
+print("a-b=",a-b)         #subtraction
+
+print("a*b=",a*b)          #multiplication
+
+print("a/b=",a/b)          #division
+
+print("a%b=",a%b)          #modulus
+
+print("a//b=",a//b)         #floor division
+
