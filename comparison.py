456347a058dbef558320d5c05020b30a5fff90e8
yamini-pant001 <yaminipant001@gmail.com>
2025-04-29 11:00:01 +0530

    Add files via upload

---
+++ comparison.py
@@ -0,0 +1,16 @@
+# comparison operators
+
+a=int(input("enter a first number"))
+b=int(input("enter a second number"))
+
+#displaying the values
+print("a=",a)
+print("b=",b)
+
+
+print(a==b)     #is equal to (==)
+print(a!=b)     #not equal to (!=)
+print(a>b)      #greater than (>)
+print(a<b)      #less than (<)
+print(a>=b)      #greater than equal (>)
+print(a<=b)      #less than equal (<)
